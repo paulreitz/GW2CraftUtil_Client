a9c16f4b23eed554a3cb9d9cee93ec86972b91b8
paulreitz <avenose@gmail.com>
2019-12-11 07:00:37 -0800

    Added retry logic to the server call

--- src/services/requestService.js
+++ src/services/requestService.js
@@ -1,18 +1,31 @@
 import axios from 'axios';
 
 const server = process.env.GW_CRAFT_ENDPOINT_URL;
+const retryTimeMs = 500;
+const maxRetries = 5;
 
 export const serverCall = (endPoint, data) => {
     return new Promise((resolve, reject) => {
-        // handle parsing data and retries later, just get a call working.
-        axios.get(`${server}${endPoint}`,{
-            params: data || {}
-        })
-        .then((response) => {
-            resolve(response.data);
-        })
-        .catch((error) => {
-            reject(error);
-        });
+        attemptCall(endPoint, data, resolve, reject, 0);
     });
+}
+
+const attemptCall = (endPoint, data, resolve, reject, attempt) => {
+    axios.get(`${server}${endPoint}`,{
+        params:data || {}
+    })
+    .then((response) => {
+        resolve(response.data);
+    })
+    .catch((error) => {
+        if (attempt < maxRetries) {
+            setTimeout(() => {
+                attempt++;
+                attemptCall(endPoint, data, resolve, reject, attempt);
+            }, attempt * retryTimeMs);
+        }
+        else {
+            reject(error);
+        }
+    })
 }

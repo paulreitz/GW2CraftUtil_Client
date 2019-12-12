47e62058db6b3a55a666357dd2e884c6cdb44dbd
paulreitz <avenose@gmail.com>
2019-12-12 06:51:33 -0800

    Set the overflow on the materials list so it doesn't scroll

--- src/components/MaterialList.js
+++ src/components/MaterialList.js
@@ -7,31 +7,35 @@ export class MaterialList extends React.Component {
         const imageSize = 32;
         return (
             <aside className="materials-list">
-                <h3 className="materials-list__title">
-                    Base Materials
-                </h3>
-                {mats.length 
-                    ? (<div className="materials-list--mats">
-                        {mats.map((mat) => (
-                            <div className={`materials-list--mats__container ${mat.item.rarity.toLowerCase()}-border`} key={mat.item.id}>
-                                <div className="materials-list--mats__count">
-                                    {mat.count}
-                                </div>
-                                <div className={`materials-list--mats__display ${mat.item.rarity.toLowerCase()}`}>
-                                    <img src={mat.item.icon} className={`materials-list--mats__icon ${mat.item.rarity.toLowerCase()}-border`} width={imageSize} height={imageSize} />
-                                    <div className="materials-list--mats__name">
-                                        {mat.item.name.replace(/\&lsquo;/g, `'`)}
+                <div className="materials-list__container">
+                    <div className="materials-list__content">
+                    <h3 className="materials-list__title">
+                        Base Materials
+                    </h3>
+                    {mats.length 
+                        ? (<div className="materials-list--mats">
+                            {mats.map((mat) => (
+                                <div className={`materials-list--mats__container ${mat.item.rarity.toLowerCase()}-border`} key={mat.item.id}>
+                                    <div className="materials-list--mats__count">
+                                        {mat.count}
+                                    </div>
+                                    <div className={`materials-list--mats__display ${mat.item.rarity.toLowerCase()}`}>
+                                        <img src={mat.item.icon} className={`materials-list--mats__icon ${mat.item.rarity.toLowerCase()}-border`} width={imageSize} height={imageSize} />
+                                        <div className="materials-list--mats__name">
+                                            {mat.item.name.replace(/\&lsquo;/g, `'`)}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
-                    </div>)
-                    : (<div className="materials-list--no-mats">
-                        <div className="materials-list--no-mats__message">
-                            <div>There are currently no materials to show.</div>
-                            <div>Select an item to view the base materials needed.</div>
-                        </div>    
-                    </div>)}
+                            ))}
+                        </div>)
+                        : (<div className="materials-list--no-mats">
+                            <div className="materials-list--no-mats__message">
+                                <div>There are currently no materials to show.</div>
+                                <div>Select an item to view the base materials needed.</div>
+                            </div>    
+                        </div>)}
+                    </div>
+                </div>
             </aside>
         );
     }
